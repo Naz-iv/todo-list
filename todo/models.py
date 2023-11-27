4d8dad293a8b0c24ebf6209f921b65111840b244
naiv <nazarivankiv1@gmail.com>
2023-11-27 15:39:52 +0200

    solution

--- todo/models.py
+++ todo/models.py
@@ -6,11 +6,14 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField(default=False)
-    tags = models.ManyToManyField("Tags", blank=True)
+    tags = models.ManyToManyField("Tag", blank=True, related_name="tasks")
 
     class Meta:
-        ordering = ["-completed", "-created_at",]
+        ordering = ["completed", "-created_at",]
 
 
-class Tags(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,61 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views import generic
 
-from todo.models import Task
+from todo.forms import TaskForm
+from todo.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
+    paginate_by = 10
+
+    queryset = Task.objects.prefetch_related('tags')
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+
+
+def toggle_complete(request: HttpRequest, pk: int) -> HttpResponse:
+    task = Task.objects.get(pk=pk)
+    if task.completed:
+        task.completed = False
+    else:
+        task.completed = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todo:task-list"))
